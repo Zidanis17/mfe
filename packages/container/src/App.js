7757ee778a1a291997454237960014d0a3f301d6
Islam Zidan <isozidan12@gmail.com>
2026-07-04 16:37:00 +0300

    feat: enhance routing and history management in MarketingApp and App components

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,8 +7,7 @@ import Header from './components/Header';
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
 });
-export default () => {
-
+export default (history) => {
     return (
         <>
             <StylesProvider generateClassName={generateClassName}>
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(({ pathname: nextPathname }) => {
+            onNavigate({ pathname: nextPathname });
+        });
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate(location) {
+            if (history.location.pathname !== location) {
+                history.push(location);
+            }
+        }
+    }
 }
 
 
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {
+            defaultHistory: createBrowserHistory()
+        });
     }
 }
 
