783ff384c7076d4131bffdadccc8ecb3c309fc9b
Islam Zidan <isozidan12@gmail.com>
2026-07-04 10:26:31 +0300

    feat: integrate StylesProvider with generateClassName in App components

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 export default () => {
 
     return (
         <>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <MarketingApp />
-                </div>
-            </BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <MarketingApp />
+                    </div>
+                </BrowserRouter>
+            </StylesProvider>
         </>
     )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import { StylesProvider } from '@material-ui/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+});
+
 export default () => {
     return <>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={Landing} />
                     <Route exact path="/pricing" component={Pricing} />
                 </Switch>
             </BrowserRouter>
-            </StylesProvider>   
+        </StylesProvider>   
 
     </>
 }
