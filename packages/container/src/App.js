a6377b8d0d619162ee0be5e8162f042f566196ac
Islam Zidan <isozidan12@gmail.com>
2026-07-04 07:38:13 +0300

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+    return (
+        <>
+        <div>Container</div>
+        <hr></hr>
+        <MarketingApp />
+        </>
+    )
+}
